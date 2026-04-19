809f23e8afa0021311434d53847ede57512020ec
Sonal Vishwakarma <drsonalvishwakarma@gmail.com>
2026-04-19 21:43:59 +0530

    Learning

--- Backend/controller/ToDoController.js
+++ Backend/controller/ToDoController.js
@@ -2,5 +2,14 @@ const ToDoModel = require("../models/ToDoModel")
 
 module.exports.getToDo = async(req, res) => {
     const toDo = await ToDoModel.find()
-    res dbhwbd 
+    res.json(toDo)
+}
+
+module.exports.createToDo = async(req, res) => {
+    const { title, description } = req.body
+    const newToDo = new ToDoModel({ title, description })
+    await newToDo.save()
+    res.json(newToDo)
+    
+    
 }
