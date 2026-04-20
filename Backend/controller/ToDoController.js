f62b8388a9f59461fd62805b9864823615881c54
Sonal Vishwakarma <drsonalvishwakarma@gmail.com>
2026-04-20 23:36:14 +0530

    learing

--- Backend/controller/ToDoController.js
+++ Backend/controller/ToDoController.js
@@ -10,6 +10,14 @@ module.exports.createToDo = async(req, res) => {
     const newToDo = new ToDoModel({ title, description })
     await newToDo.save()
     res.json(newToDo)
+
+
     
-    
+}
+module.exports.createToDo = module.exports.createToDo
+module.exports.updateToDo = async(req, res) => {
+    const { id } = req.params
+    const { title, description } = req.body
+    const updatedToDo = await ToDoModel.findByIdAndUpdate(id, { title, description }, { new: true })
+    res.json(updatedToDo)
 }
